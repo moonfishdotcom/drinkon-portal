92a8ecdb486a91b5993bc765f9fba0c356fff2d5
timharris123 <tharris_hm@hotmail.com>
2014-11-05 07:20:56 +0000

    (#33) Added initial design for the specials page

--- app_code/app_controllers/details/vendorProductSpecialsController.js
+++ app_code/app_controllers/details/vendorProductSpecialsController.js
@@ -1,4 +1,28 @@
 drinkon.controller('vendorProductSpecialsController', function($scope, dbRepository)
 {
+//  var _vendor_id = "1";
+  var _vendor_id = __cookie_vendor_id;
+
+/*
+  dbRepository.getVendorProductsList(_vendor_id, function(_error, _data)
+  {
+    $scope.items = _data.Data;
+    console.log($scope.items);
+  });
+*/
+
+  dbRepository.getVendorProductLinesList(_vendor_id, function(_error, _data)
+  {
+    $scope.items = _data.Data;
+    console.log($scope.items);
+  });
+
+  dbRepository.getVendorProductTypesList(_vendor_id, function(_error, _data)
+  {
+    $scope.product_types = _data.Data;
+    console.log($scope.product_types);
+  });
+
+
 
 });
